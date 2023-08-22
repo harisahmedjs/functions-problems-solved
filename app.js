b2eaae0168b746fb30eafb751692320c8da30d73
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2023-08-22 15:16:56 +0500

    problems solved

---
+++ app.js
@@ -0,0 +1,62 @@
+
+// function sumOFNumber(a , b){
+//     let sum = a + b;
+    // console.log((sum));
+//     return sum
+// }
+
+// let firstNumber = +prompt("Enter a First Number");
+// let secondNumber = +prompt("Enter a Second Number");
+// const totalNumber = (firstNumber + secondNumber)
+// console.log(sumOFNumber(firstNumber,secondNumber));
+
+
+//  function name(c , d){
+//     let nameadd= c + d;
+//      return nameadd;
+//  }
+// let name1= prompt("enter your first name");
+// let name2= prompt("enter your last name");
+// console.log(nam(name1 + "" , name2));
+
+
+// function calculator1(num1 , operator , num2) { 
+//     switch (operator) {
+//         case ("+"):
+//            let sum =  num1 + num2
+//            console.log(sum)
+//             break;
+//         case "-":
+//            let sub = num1 - num2
+//            console.log(sub)
+
+//             break;
+//         case "*":
+//           let  multiply  = num1 * num2
+//           console.log(multiply)
+
+//             break;
+//         case "/":
+//             let divide = num1 / num2
+//            console.log(divide)
+
+//             break;
+    
+//         default:
+//             document.write("enter a correct input")
+//             return "Enter correct ";
+//         }
+       
+//     }
+// let num = +prompt("enter a number")
+// let operator = prompt("enter a operator")
+// let num1 = +prompt("enter a number")
+// console.log(calculator1(num , operator , num1));
+//  Q no 5
+// function numberSquir (number , number){
+// let number1 = number*number
+// return number1
+// }
+// let number1 =("2")
+// let number2 = +prompt("Enter number")
+// console.log(numberSquir(number1 , number2));
